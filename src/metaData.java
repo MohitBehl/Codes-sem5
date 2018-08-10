f279acc26a577bcdf5908fb17d88b94b767d38c9
MohitBehl <mohitbehlofficial@gmail.com>
2018-08-10 10:06:29 +0530

    updates

--- src/metaData.java
+++ src/metaData.java
@@ -6,20 +6,39 @@ public class metaData {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		String path = scn.next();
-		File obj = new File(path);
-		if (obj.exists()) {
-			System.out.println(path + " :exists");
-			System.out.println(obj.isDirectory() ? "directory path found" : "file path found");
-			if (obj.isDirectory()) {
-				System.out.println("Directory Listing:");
-				String[] arr = obj.list();
-				for (String str : arr) {
-					System.out.println(str);
-				}
-
-			}
-		}
+		solve(path);
 		scn.close();
 	}
 
+	private static void solve(String pathName) {
+		File obj = new File(pathName);
+		if (obj.exists()) {
+			System.out.println(pathName + " :exists");
+			if (obj.isDirectory()) {
+				System.out.println("directory path found");
+				directoryHandler(obj);
+			} else {
+				System.out.println("file path found");
+				fileHandler(obj);
+			}
+		} else
+			System.out.println(pathName + " :doesn't exist");
+	}
+
+	private static void directoryHandler(File obj) {
+		System.out.println("Directory Listing:");
+		String[] arr = obj.list();
+		for (String str : arr) {
+			System.out.println(str);
+		}
+		commonHandler(obj);
+	}
+
+	private static void fileHandler(File obj) {
+		commonHandler(obj);
+	}
+
+	private static void commonHandler(File obj) {
+
+	}
 }
