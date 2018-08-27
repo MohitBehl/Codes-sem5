10acda99d49396ce558c7e825d5050ba940c8785
MohitBehl <mohitbehlofficial@gmail.com>
2018-08-27 16:22:25 +0530

    updated dynamic rmi

--- src/rmi/Server.java
+++ src/rmi/Server.java
@@ -1,8 +1,9 @@
 package rmi;
 
-import java.awt.image.AreaAveragingScaleFilter;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -26,59 +27,83 @@ public class Server {
 
 	}
 
-	void invoke(String message) throws Exception {
+	void invoke(String message) {
 		String[] arr = message.split("-");
 
 		String className = arr[0];
 		String methodType = arr[1];
 		String methodName = arr[2];
 		int numOfargs = Integer.parseInt(arr[3]);
+		String errorMessage = null;
 
-		Class<?> anomynous = Class.forName(arr[0]);
-		Class<?> parameterTypes[] = null;
-		Object parameterValue[] = null;
-		if (numOfargs > 0) {
-			parameterTypes = new Class[numOfargs];
-			parameterValue = new Object[numOfargs];
-			int i = 4;
-			int j = 0;
-			while (i < arr.length) {
-				String type = arr[i];
-				if (type.equals("int")) {
-					parameterTypes[j] = int.class;
-					parameterValue[j] = Integer.parseInt(arr[i + 1]);
-				} else if (type.equals("double")) {
-					parameterTypes[j] = double.class;
-					parameterValue[j] = Double.parseDouble(arr[i + 1]);
-				} else if (type.equals("float")) {
-					parameterTypes[j] = float.class;
-					parameterValue[j] = Double.parseDouble(arr[i + 1]);
-				} else if (type.equals("boolean")) {
-					parameterTypes[j] = boolean.class;
-					parameterValue[j] = Boolean.parseBoolean(arr[i + 1]);
-				} else if (type.equals("char")) {
-					parameterTypes[j] = char.class;
-					parameterValue[j] = arr[i + 1].charAt(0);
-				} else if (type.equals("String")) {
-					parameterTypes[j] = String.class;
-					parameterValue[j] = arr[i + 1];
-				} else
-					throw new Exception("invalid type found");
-				i += 2;
-				j++;
+		try {
+			Class<?> anomynous = Class.forName(className);
+			Class<?> parameterTypes[] = null;
+			Object parameterValue[] = null;
+
+			if (numOfargs > 0) {
+				parameterTypes = new Class[numOfargs];
+				parameterValue = new Object[numOfargs];
+				int i = 4;
+				int j = 0;
+				while (i < arr.length) {
+					String type = arr[i];
+					if (type.equals("int")) {
+						parameterTypes[j] = int.class;
+						parameterValue[j] = Integer.parseInt(arr[i + 1]);
+					} else if (type.equals("double")) {
+						parameterTypes[j] = double.class;
+						parameterValue[j] = Double.parseDouble(arr[i + 1]);
+					} else if (type.equals("float")) {
+						parameterTypes[j] = float.class;
+						parameterValue[j] = Double.parseDouble(arr[i + 1]);
+					} else if (type.equals("boolean")) {
+						parameterTypes[j] = boolean.class;
+						parameterValue[j] = Boolean.parseBoolean(arr[i + 1]);
+					} else if (type.equals("char")) {
+						parameterTypes[j] = char.class;
+						parameterValue[j] = arr[i + 1].charAt(0);
+					} else if (type.equals("String")) {
+						parameterTypes[j] = String.class;
+						parameterValue[j] = arr[i + 1];
+					} else
+						out.writeUTF("invalid type found");
+					i += 2;
+					j++;
+				}
+			}
+			Object obj = anomynous.newInstance();
+			Method method = obj.getClass().getMethod(methodName, parameterTypes);
+			// method calling is made static , will come up with some solution
+			// soon
+			// out.writeUTF(method.invoke(obj, Integer.parseInt(arr[5])) + "");
+			out.writeUTF(method.invoke(obj, parameterValue) + "");
+		} catch (ClassNotFoundException e) {
+			errorMessage = "no such class found";
+		} catch (IOException e) {
+			errorMessage = "invalid message format";
+		} catch (IllegalAccessException e) {
+			errorMessage = e + "";
+		} catch (InstantiationException e) {
+			errorMessage = "class cannot be instantiatized";
+		} catch (NoSuchMethodException e) {
+			errorMessage = "no such method exists";
+		} catch (InvocationTargetException e) {
+			errorMessage = "invalid agrument format";
+		}
+		if (errorMessage != null) {
+			try {
+				out.writeUTF(errorMessage);
+			} catch (IOException e) {
+				System.out.println("error cannot be audited");
 			}
 		}
-		Object obj = anomynous.newInstance();
-		Method method = obj.getClass().getMethod(methodName, parameterTypes);
-		// method calling is made static , will come up with some solution soon
-		// out.writeUTF(method.invoke(obj, Integer.parseInt(arr[5])) + "");
-		out.writeUTF(method.invoke(obj, parameterValue) + "");
 	}
 
 	public static void main(String args[]) {
 		int port;
 		Scanner scn = new Scanner(System.in);
-		System.out.println("enter post number:");
+		System.out.println("enter port number:");
 		port = scn.nextInt();
 		try {
 			Server server = new Server(port);
